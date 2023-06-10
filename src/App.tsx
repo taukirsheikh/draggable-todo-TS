10b6519f612a9daf7295af8ad33db0fc5748ba30
taukirsheikh <taukirsheikh2@gmail.com>
2023-06-10 12:35:25 +0545

    bg added

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+const App: React.FC = () => {
+
+
+  return (
+<>
+    
+      <div className='app'>
+        <span className="heading">TASKIFY</span>
+        
+      </div>
+</>
+    
+
+  )
+}
+
+export default App
